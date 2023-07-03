2025a65d8606f295ee80750939874ded2c3d094a
00drdelius <1092104983@qq.com>
2023-07-04 07:43:45 +0800

    modified:   run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 deepspeed --include localhost:0 src/train_lomo.py config/args_lomo.yaml
 
 # for LoRA + LOMO
-#deepspeed --master_port "$port" --include localhost:0 src/train_lomo_lora.py config/args_lomo_lora.yaml
+#deepspeed --include localhost:0 src/train_lomo_lora.py config/args_lomo_lora.yaml
