56823c988eb10879cfa0882524892dc8c4bd38e1
00drdelius <1092104983@qq.com>
2023-07-06 11:23:32 +0800

    new file:   config/args_lomo_qlora.yaml 	modified:   run.sh

--- run.sh
+++ run.sh
@@ -3,3 +3,6 @@ deepspeed --include localhost:0 src/train_lomo.py config/args_lomo.yaml
 
 # for LoRA + LOMO
 #deepspeed --include localhost:0 src/train_lomo_lora.py config/args_lomo_lora.yaml
+
+# for qLoRA + LOMO
+#python src/train_lomo_qlora.py config/args_lomo_qlora.yaml
