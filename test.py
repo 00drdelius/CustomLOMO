b9a7ac98169ae6e708c42e9deefb3bb33d43e668
00drdelius <1092104983@qq.com>
2023-07-20 02:27:35 +0800

    deleted:    assets/LOMO.png 	deleted:    assets/hook_func.png 	deleted:    baichuanConfig/tokenization_baichuan.py 	deleted:    baichuanConfig/tokenizer.model 	deleted:    baichuanConfig/tokenizer_config.json 	deleted:    src/QLoRACollator.py 	deleted:    src/lomo_qlora_trainer.py 	deleted:    src/train_qlora.py 	modified:   test.py

--- test.py
+++ test.py
@@ -1,34 +1,43 @@
 import torch
 
-with torch.no_grad():
-    # 模型初始化
-    linear1 = torch.nn.Linear(1024,1024, bias=False).cuda() # + 4194304(4096KB)
-    print(torch.cuda.memory_allocated()/(1024))   #4194304   4096KB
-    linear2 = torch.nn.Linear(1024, 1, bias=False).cuda() # + 4096(4KB)
-    print(torch.cuda.memory_allocated()/(1024))   #4198400   4100KB
 
-    # 输入定义
-    inputs = torch.tensor([[1.0]*1024]*1024).cuda() # shape = (1024,1024) # + 4194304(4096KB)
-    print(torch.cuda.memory_allocated()/(1024))   #8392704   8196KB
+# 模型初始化
+linear1 = torch.nn.Linear(1024,1024, bias=False).cuda() # + 4194304(4096KB)
+print(torch.cuda.memory_allocated()/(1024))   #4194304   4096KB
+linear2 = torch.nn.Linear(1024, 1, bias=False).cuda() # + 4096(4KB)
+print(torch.cuda.memory_allocated()/(1024))   #4198400   4100KB
 
-    # 前向传播
-    # loss = sum(linear2(linear1(inputs)))  #shape = (1) # memory + 4194304(4096KB) + 512(0.5KB)
-    # print(torch.cuda.memory_allocated()/(1024))   #21107200  20612KB
-    y = linear1(inputs)
-    print("cache: %r" % (torch.cuda.memory_reserved()/1024))
-    print(torch.cuda.memory_allocated()/(1024))
-    print(f"max: {torch.cuda.max_memory_allocated()/1024}")
-    u = linear2(y)
-    print(torch.cuda.memory_allocated()/(1024))
-    loss = sum(u)
-    print(torch.cuda.memory_allocated()/(1024))
 
-# # 后向传播
-# loss.backward() # memory - 4194304 + 4194304 + 4096
-# print(torch.cuda.memory_allocated()/(1024))
+# 输入定义
+inputs = torch.tensor([[1.0]*1024]*1024).cuda() # shape = (1024,1024) # + 4194304(4096KB)
+print(torch.cuda.memory_allocated()/(1024))   #8392704   8196KB
 
-# # 再来一次~
-# loss = sum(linear2(linear1(inputs))) # shape = (1) # memory + 4194304  (512没了，因为loss的ref还在)
-# print(torch.cuda.memory_allocated()/(1024))
-# loss.backward() # memory - 4194304
-# print(torch.cuda.memory_allocated()/(1024))
+
+# 前向传播
+# loss = sum(linear2(linear1(inputs)))  #shape = (1) # memory + 4194304(4096KB) + 512(0.5KB)
+# print(torch.cuda.memory_allocated()/(1024))   #21107200  20612.5KB
+# y = linear1(inputs)
+y = linear1(inputs)
+print(torch.cuda.memory_allocated()/(1024))   #20612KB
+u = linear2(y)
+print(torch.cuda.memory_allocated()/(1024))   #20616KB
+loss = sum(u)
+print(torch.cuda.memory_allocated()/(1024))   #20616.5KB
+
+# 后向传播
+loss.backward() # memory - 4194304 + 4194304 + 4096
+print(torch.cuda.memory_allocated()/(1024))   #33036.5KB
+print(torch.cuda.max_memory_allocated()/(1024))  #37133
+
+print(list(linear1.parameters())[0].grad.shape)
+print(list(linear2.parameters())[0].grad.shape)
+list(linear1.parameters())[0].grad = None
+list(linear2.parameters())[0].grad = None
+
+
+# 再来一次~
+loss = sum(linear2(linear1(inputs))) # shape = (1) # memory + 4194304  (512没了，因为loss的ref还在)
+print(torch.cuda.memory_allocated()/(1024))   #37132.5KB  with parameters.grad set to zero => 33032.5 = 37132.5-4096-4
+loss.backward() # memory - 4194304
+print(torch.cuda.memory_allocated()/(1024))   #33036.5KB
+print(torch.cuda.max_memory_allocated()/(1024))
