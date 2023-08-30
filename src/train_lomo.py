1829b73ccb9133c448341abb3dc45588241b3b42
御前 <1092104983@qq.com>
2023-08-31 04:45:44 +0800

    modified:   src/train_lomo.py

--- src/train_lomo.py
+++ src/train_lomo.py
@@ -48,7 +48,8 @@ def train():
         torch.set_default_dtype(torch.float16)
     elif training_args.bf16 and training_args.fp16:
         torch.set_default_dtype(torch.float16)
-
+    print("default type:", torch.get_default_dtype())
+    
     model_name = model_args.model_name_or_path.split('/')[-1]
     tag_name = '_'.join([data_args.dataset_name, model_name, training_args.tag] if training_args.tag else [data_args.dataset_name, model_name])
     hparam_name = 'output'
